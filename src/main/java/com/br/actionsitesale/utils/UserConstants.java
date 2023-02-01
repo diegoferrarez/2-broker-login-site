b538724455b07a161efd3810c00f27ce66d9de3a
Alex Sacramento <alexsacramento@ext.cvccorp.com.br>
2023-02-01 16:34:37 -0300

    inserção do feign

---
+++ src/main/java/com/br/actionsitesale/utils/UserConstants.java
@@ -0,0 +1,10 @@
+package com.br.actionsitesale.utils;
+
+public class UserConstants {
+    public static final String USER_SIGN_HEADER = "gtw-user-header";
+    public static final String USER_PASS_HEADER = "gtw-pass-header";
+
+    private UserConstants(){
+
+    }
+}
